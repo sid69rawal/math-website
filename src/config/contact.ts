1c2edeac5b52b20e03ca2487cde7eeeba8f6b96a
rawals <rawals.info@gmail.com>
2025-10-05 15:47:17 -0400

    fix: correct Facebook profile link in contact configuration

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -11,7 +11,7 @@ export const contactConfig = {
     full: "1426 Pate Court\nMississauga, ON L5V 2Y8\nCanada"
   },
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=61581712106555",
+    facebook: "https://www.facebook.com/profile.php?id=61581742833856",
     instagram: "https://instagram.com/levelupmathacademy"
   }
 } as const;
